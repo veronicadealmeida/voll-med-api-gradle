59204cad033aac8d54484dbec00b2e569537b8cc
Veronica <almeida.vrc@gmail.com>
2023-09-23 08:41:19 -0300

    Remove autenticação do swagger

--- src/main/java/eduvdavollmedapi/springdoc/SpringDocConfigurations.java
+++ src/main/java/eduvdavollmedapi/springdoc/SpringDocConfigurations.java
@@ -20,24 +20,24 @@ public class SpringDocConfigurations {
 //                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
 //    }
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes("bearer-key",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")))
-                .info(new Info()
-                        .title("Voll.med API")
-                        .description("API Rest da aplicação Voll.med, contendo as funcionalidades de CRUD de médicos e de pacientes, além de agendamento e cancelamento de consultas")
-                        .contact(new Contact()
-                                .name("Time Backend")
-                                .email("backend@voll.med"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("http://voll.med/api/licenca")));
-    }
+//    @Bean
+//    public OpenAPI customOpenAPI() {
+//        return new OpenAPI()
+//                .components(new Components()
+//                        .addSecuritySchemes("bearer-key",
+//                                new SecurityScheme()
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("bearer")
+//                                        .bearerFormat("JWT")))
+//                .info(new Info()
+//                        .title("Voll.med API")
+//                        .description("API Rest da aplicação Voll.med, contendo as funcionalidades de CRUD de médicos e de pacientes, além de agendamento e cancelamento de consultas")
+//                        .contact(new Contact()
+//                                .name("Time Backend")
+//                                .email("backend@voll.med"))
+//                        .license(new License()
+//                                .name("Apache 2.0")
+//                                .url("http://voll.med/api/licenca")));
+//    }
 }
 
